20a35dde78e5851695c79753050499543f37e245
AlexNesvit <alex.nesvit2000@gmail.com>
2024-12-17 11:12:52 +0100

    ajout component Main

--- react-api/src/App.jsx
+++ react-api/src/App.jsx
@@ -3,18 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import getFlags from './api/get_flags'
+import Header from './components/header/Header'
+import Main from './components/main/Main'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Header />
+    <Main />
+
     </>
   )
 }
---
+++ react-api/src/components/main/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <main>
+                <h2>Selectionez votre région</h2>
+                <select>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                </select>
+        </main>
+    )
+}
+
+export default Main
