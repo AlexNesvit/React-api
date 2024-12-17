48dacdf083274938898a9fbca816ff578d629f12
AlexNesvit <alex.nesvit2000@gmail.com>
2024-12-17 11:49:22 +0100

    ajout component Main V.3

--- react-api/src/components/main/Main.jsx
+++ react-api/src/components/main/Main.jsx
@@ -3,13 +3,14 @@ import React from 'react'
 
 const Main = ({ countries }) => {
     const uniqueRegions = [...new Set(countries.map(country => country.region))];
+    console.log(uniqueRegions)
     return (
         <>
             <main>
                 <h2>Selectionez votre région</h2>
 
-
-                <select>
+                <label htmlFor="region">Sélectionnez votre région</label>
+                <select id="region">
                     {uniqueRegions.map((region, index) => (
                         <option key={index} value={region}>
                             {region}
